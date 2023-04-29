60463610a70e49ffaf85af9259f5fe0f411491c4
Cauan <cauanvictor0325@gmail.com>
2023-04-29 16:23:35 -0300

    feat: adding favicon

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -3,6 +3,13 @@ import localFont from 'next/font/local';
 
 const myFont = localFont({ src: './Inter.ttf' });
 
+export const metadata = {
+  title: 'NormaLabs',
+  icons: {
+    icon: '/favicon.png',
+  },
+}
+
 export default function RootLayout({ children }: {
   children: React.ReactNode;
 }) {
